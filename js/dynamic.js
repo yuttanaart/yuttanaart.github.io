47607785d81ac58c6fed738c85e0c126917f79e4
Fromz Prajuktra <pkfrom@users.noreply.github.com>
2026-04-10 12:45:51 +0700

    Create dynamic.js

---
+++ js/dynamic.js
@@ -0,0 +1,307 @@
+/**
+ * Dynamic renderer for YuttanaArt
+ * Fetches data.json and renders all sections — no hardcoded content in HTML
+ */
+(function ($) {
+  'use strict';
+
+  /* ===== FETCH DATA & BOOT ===== */
+  $.getJSON('data.json')
+    .done(function (data) {
+      renderAbout(data.about);
+      renderSlides(data.slides);
+      renderServiceIcons(data.services);
+      renderServiceImages(data.serviceImages);
+      renderBoon(data.boon, data.carouselImages);
+      renderMap(data.map);
+      renderParallax(data.parallax);
+      renderFooter(data.branches, data.contacts, data.site);
+      renderSocial(data.site);
+      initPlugins();
+      initScrollBehaviors();
+    })
+    .fail(function () {
+      console.error('ไม่สามารถโหลด data.json ได้');
+    });
+
+  /* ===== RENDER: ABOUT ===== */
+  function renderAbout(about) {
+    var features = about.features.map(function (f) {
+      return '<li><i class="fa fa-check"></i>' + f + '</li>';
+    }).join('');
+
+    $('#about-content').html(
+      '<div class="row">' +
+        '<div class="col-sm-6 padd">' +
+          '<h4 class="uppercase">' + about.heading + '</h4>' +
+          '<div class="line-left"></div>' +
+          '<p class="weight-600">' + about.subheading + '</p>' +
+          '<ul class="list3">' + features + '</ul>' +
+          '<br><p class="weight-400">' + about.extra + '</p><br>' +
+          '<a href="#" class="btn rounded btn-lg pi-btn-default">เปิด 24 ชั่วโมง</a>' +
+        '</div>' +
+        '<div class="col-sm-6 padd">' +
+          '<img class="img-responsive wow zoomIn" src="' + about.image + '" />' +
+        '</div>' +
+      '</div>'
+    );
+  }
+
+  /* ===== RENDER: SLIDES ===== */
+  function renderSlides(slides) {
+    var html = slides.map(function (slide, i) {
+      var inner = '';
+      if (slide.heading) {
+        inner += '<p class="ls-l" style="top:140px;left:125px;font-weight:300;height:40px;padding-right:10px;padding-left:10px;font-size:36px;line-height:37px;color:#fff;white-space:nowrap;" data-ls="offsetxin:0;durationin:2000;delayin:1500;easingin:easeOutElastic;rotatexin:90;transformoriginin:50% top 0;offsetxout:-600;">' + slide.heading + '</p>';
+      }
+      if (slide.badge) {
+        inner += '<a class="btn pi-btn-default ls-s-1" href="#" style="padding:9px 20px;font-size:20px;top:240px;left:135px;white-space:nowrap;" data-ls="offsetxin:0;durationin:2000;delayin:2000;easingin:easeOutElastic;rotatexin:-90;transformoriginin:50% top 0;offsetxout:-200;durationout:1000;parallaxlevel:3;">' + slide.badge + '</a>';
+      }
+      if (slide.lines) {
+        slide.lines.forEach(function (line, li) {
+          inner += '<p class="ls-l" style="top:' + (80 + li * 90) + 'px;left:730px;font-size:22px;color:#fff;white-space:nowrap;" data-ls="offsetxin:0;durationin:2000;delayin:' + (1500 + li * 500) + ';easingin:easeOutElastic;">' + line + '</p>';
+        });
+      }
+      var nextSlide = i === 0 ? 2 : 1;
+      inner +=
+        '<img class="ls-l ls-linkto-' + nextSlide + '" style="top:320px;left:135px;" data-ls="offsetxin:-50;delayin:1000;offsetxout:-50;parallaxlevel:3;" src="sliderimages/left.png">' +
+        '<img class="ls-l ls-linkto-' + nextSlide + '" style="top:320px;left:175px;" data-ls="offsetxin:50;delayin:1000;offsetxout:50;parallaxlevel:3;" src="sliderimages/right.png">';
+
+      return '<div class="ls-slide" data-ls="transition2d:95;timeshift:-1000;"><img src="' + slide.bg + '" class="ls-bg" alt=""/>' + inner + '</div>';
+    }).join('');
+
+    $('#layerslider').html(html);
+  }
+
+  /* ===== RENDER: SERVICE ICONS ===== */
+  function renderServiceIcons(services) {
+    var delays = [1, 2, 3, 4];
+    var html = services.map(function (s, i) {
+      return (
+        '<div class="col-sm-6 col-md-3 padd">' +
+          '<div class="single_box style6 wow fadeIn" data-wow-duration="' + delays[i] + 's">' +
+            '<div class="icons_introimg image-icon box ' + s.colorClass + '">' +
+              '<i class="fa ' + s.icon + '"></i>' +
+            '</div>' +
+            '<h3>' + s.title + '</h3>' +
+            '<p class="introtext">' + s.description + '</p>' +
+          '</div>' +
+        '</div>'
+      );
+    }).join('');
+    $('#service-icons').html(html);
+  }
+
+  /* ===== RENDER: SERVICE IMAGES ===== */
+  function renderServiceImages(images) {
+    var html = images.map(function (img, i) {
+      return (
+        '<div class="col-sm-4 padd wow fadeIn" data-wow-duration="' + (i + 1) + 's">' +
+          '<img class="img-responsive margin-bottom-25" src="' + img.src + '" alt="' + img.alt + '" />' +
+        '</div>'
+      );
+    }).join('');
+    $('#service-images').html(html);
+  }
+
+  /* ===== RENDER: BOON ===== */
+  function renderBoon(boon, carouselImages) {
+    var carouselItems = carouselImages.map(function (src) {
+      return '<div class="item"><img src="' + src + '" /></div>';
+    }).join('');
+
+    var boonItems = boon.items.map(function (item) {
+      return '<li><i class="fa fa-check"></i>' + item + '</li>';
+    }).join('');
+
+    $('#boon-content').html(
+      '<div class="row">' +
+        '<div class="col-sm-12 col-md-6 padd">' +
+          '<div class="owl-carousel owl-item-1">' + carouselItems + '</div>' +
+        '</div>' +
+        '<div class="col-sm-12 col-md-6 padd">' +
+          '<h5 class="uppercase">' + boon.heading + '</h5>' +
+          '<div class="line-left"></div>' +
+          '<ul class="list3">' + boonItems + '</ul>' +
+          '<br><a href="#boon" class="btn rounded btn-lg pi-btn-default">ให้กับลูกค้าที่สนใจบริการด้วยค่ะ</a>' +
+        '</div>' +
+      '</div>'
+    );
+  }
+
+  /* ===== RENDER: MAP ===== */
+  function renderMap(map) {
+    $('#map-content').html(
+      '<iframe src="' + map.embedUrl + '" width="100%" height="400" frameborder="0" style="border:0;border-radius:12px;" allowfullscreen=""></iframe>'
+    );
+  }
+
+  /* ===== RENDER: PARALLAX ===== */
+  function renderParallax(parallax) {
+    var branchItems = parallax.branches.map(function (b) {
+      return '<li><i class="fa fa-map-marker"></i>' + b + '</li>';
+    }).join('');
+
+    $('#parallax-content').html(
+      '<h3>' + parallax.heading + '</h3>' +
+      '<p>' + parallax.description + '</p>' +
+      '<ul class="list3">' + branchItems + '</ul>' +
+      '<a href="#map" class="btn rounded btn-lg pi-btn-default">เปิด 24 ชั่วโมง</a>'
+    );
+  }
+
+  /* ===== RENDER: FOOTER ===== */
+  function renderFooter(branches, contacts, site) {
+    var branchCols = branches.map(function (b) {
+      var phones = b.phones.map(function (p) {
+        return '<i class="fa fa-phone"></i>&nbsp;<a href="' + p.href + '">' + p.number + '</a><br>';
+      }).join('');
+
+      return (
+        '<div class="col-sm-4 padd">' +
+          '<div class="icon"><i class="fa fa-map-marker"></i></div>' +
+          '<h5>' + b.label + '</h5>' +
+          '<address><p>' +
+            '<i class="fa fa-map-marker"></i>&nbsp;' + b.address + '<br>' +
+            (b.note ? b.note + '<br>' : '') +
+            '<i class="fa fa-map-marker"></i>&nbsp;<a href="' + b.mapUrl + '" target="_blank">Google Map</a><br><br>' +
+            phones +
+          '</p></address>' +
+        '</div>'
+      );
+    }).join('');
+
+    var contactLines = contacts.map(function (c) {
+      return (
+        '<i class="fa fa-phone"></i>&nbsp;<a href="' + c.href + '">' + c.phone + '</a><br>' +
+        '<i class="c-fa-line"></i>&nbsp; Line ID : <a href="' + c.lineUrl + '">' + c.lineId + '</a><br><br>'
+      );
+    }).join('');
+
+    var contactCol =
+      '<div class="col-sm-4 padd">' +
+        '<div class="icon"><i class="fa fa-envelope"></i></div>' +
+        '<h5>ติดต่อ</h5>' +
+        contactLines +
+        '<div class="fb-like" data-href="https://www.facebook.com/' + site.facebook.split('/').pop() + '" data-layout="button_count" data-action="like" data-show-faces="true" data-share="false"></div>' +
+      '</div>';
+
+    $('#footer-branches').html(branchCols + contactCol);
+  }
+
+  /* ===== RENDER: SOCIAL ICONS ===== */
+  function renderSocial(site) {
+    $('#footer-social').html(
+      '<a href="' + site.facebook + '" target="_blank" class="social-icon-jump-x4 circle">' +
+        '<div>' +
+          '<i class="fa fa-facebook facebook-icon-jump"></i>' +
+          '<i class="fa fa-facebook social-icon-jump-dark"></i>' +
+        '</div>' +
+      '</a>'
+    );
+  }
+
+  /* ===== INIT JQUERY PLUGINS (after DOM is ready) ===== */
+  function initPlugins() {
+    // LayerSlider
+    $('#layerslider').layerSlider({
+      responsive: false,
+      responsiveUnder: 1280,
+      layersContainer: 1280,
+      skin: 'noskin',
+      hoverPrevNext: false
+    });
+
+    // Owl Carousel
+    $('.owl-carousel.owl-item-1').owlCarousel({
+      singleItem: true,
+      autoPlay: true,
+      navigation: false,
+      pagination: true
+    });
+
+    // WOW animations
+    new WOW().init();
+
+    // Parallax
+    if ($.fn.parallax) {
+      $('.parallaxBg').parallax('50%', 0.3);
+    }
+
+    // Scroll to top
+    if ($.fn.UItoTop) {
+      $().UItoTop({ easingType: 'easeOutQuart' });
+    }
+  }
+
+  /* ===== SCROLL BEHAVIORS ===== */
+  function initScrollBehaviors() {
+    // Navbar scroll effect
+    function handleNavbarScroll() {
+      if ($(window).scrollTop() > 60) {
+        $('.navbar.navbar-default').addClass('navbar-scrolled');
+      } else {
+        $('.navbar.navbar-default').removeClass('navbar-scrolled');
+      }
+    }
+    $(window).on('scroll.navbar', handleNavbarScroll);
+    handleNavbarScroll();
+
+    // Smooth scroll for anchor links
+    $(document).on('click', 'a[href^="#"]', function (e) {
+      var target = $(this.getAttribute('href'));
+      if (target.length) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: target.offset().top - 70 }, 600);
+      }
+    });
+
+    // Scroll reveal
+    function revealOnScroll() {
+      var windowBottom = $(window).scrollTop() + $(window).height();
+      $('.reveal, .reveal-left').each(function () {
+        if (windowBottom > $(this).offset().top + 60) {
+          $(this).addClass('visible');
+        }
+      });
+    }
+
+    // Add reveal classes to sections after render
+    setTimeout(function () {
+      $('.single_box').each(function (i) {
+        $(this).addClass('reveal').css('transition-delay', (i * 0.12) + 's');
+      });
+      $('.col-sm-4.padd').each(function (i) {
+        $(this).addClass('reveal').css('transition-delay', (i * 0.15) + 's');
+      });
+      $('#about-content .col-sm-6').first().addClass('reveal-left');
+      $('#about-content .col-sm-6').last().addClass('reveal');
+      $('#boon-content .col-sm-12, #boon-content .col-sm-6').each(function (i) {
+        $(this).addClass('reveal').css('transition-delay', (i * 0.15) + 's');
+      });
+      $('#footer-branches .col-sm-4').each(function (i) {
+        $(this).addClass('reveal').css('transition-delay', (i * 0.1) + 's');
+      });
+      revealOnScroll();
+    }, 100);
+
+    $(window).on('scroll.reveal', revealOnScroll);
+
+    // Active nav link on scroll
+    $(window).on('scroll.activeLink', function () {
+      var scrollPos = $(window).scrollTop() + 100;
+      $('section[id], footer[id]').each(function () {
+        var id = '#' + $(this).attr('id');
+        var top = $(this).offset().top;
+        var bottom = top + $(this).outerHeight();
+        var link = $('.zetta-menu li a[href="' + id + '"]').parent();
+        if (scrollPos >= top && scrollPos < bottom) {
+          link.addClass('active');
+        } else {
+          link.removeClass('active');
+        }
+      });
+    });
+  }
+
+}(jQuery));
